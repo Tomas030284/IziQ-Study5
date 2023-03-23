c0d18de7bc89785b78ae35d2b7f98c4af402dce0
Tomas030284 <tomas.mogrovejo.acosta@gmail.com>
2023-03-22 22:23:34 -0500

    Deployment

--- src/pages/api/supabaseClient.js
+++ src/pages/api/supabaseClient.js
@@ -1,6 +1,6 @@
 import { cliente, createClient } from '@supabase/supabase-js'
 
-const supabaseURL = "https://mckdtyupusnhcabyhyja.supabase.co"; //process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = "NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY";//process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY;
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY;
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
